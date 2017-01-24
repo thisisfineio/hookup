c731e444d1c17c5794773a928524162a62432301
Chris Cooper <christopher_cooper@harvard.edu>
2017-01-24 14:45:25 -0500

    adds base functions for interacting with aws direct connect

--- hookup.go
+++ hookup.go
@@ -1,18 +1,51 @@
 package hookup
 
+import (
+	"github.com/aws/aws-sdk-go/service/directconnect"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/thisisfineio/common"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"errors"
+)
+
+
+
 type DirectConnect interface {
 	DescribeDirectConnects(*DescribeDirectConnectsInput) (*DescribeDirectConnectsOutput, error)
 }
 
 type DescribeDirectConnectsInput struct {
-
+	AwsInput *directconnect.DescribeConnectionsInput
 }
 
 type DescribeDirectConnectsOutput struct {
 
 }
 
+var (
+	NoValidProviderErr = errors.New("hookup: No valid provider was given to NewDirectConnect()")
+)
+
+func NewDirectConnect(provider int, region string) (DirectConnect, error) {
+	switch provider {
+	case common.AwsProvider:
+		return DirectConnect(NewAwsDirectConnect(region)), nil
+	}
+	return nil, NoValidProviderErr
+}
+
+func NewAwsDirectConnectWithConfig(c *aws.Config) *AwsDirectConnect {
+	return &AwsDirectConnect{directconnect.New(session.New(c))}
+}
 
 type AwsDirectConnect struct {
+	service *directconnect.DirectConnect
+}
 
+func NewAwsDirectConnect(region string) *AwsDirectConnect {
+	return NewAwsDirectConnectWithConfig(&aws.Config{Region: aws.String(region)})
+}
+
+func (a *AwsDirectConnect) DescribeDirectConnects(input *DescribeDirectConnectsInput) (*DescribeDirectConnectsOutput, error) {
+	return nil, nil
 }
